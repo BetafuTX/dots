da517ea02cd4fa8848b228d99438c28242ed684a
TXFlank <betafu@gmail.com>
2025-11-27 14:10:10 -0600

    updated new dots

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,3 @@
-#set -x ZELLIJ_AUTO_EXIT true
-#if status is-interactive
-    # Commands to run in interactive sessions can go here
-#    eval (zellij setup --generate-auto-start fish | string collect)
 
 fastfetch --kitty-icat /home/beta/Dropbox/Avatars/cap-cat.jpg
 
